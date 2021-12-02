6aefae4de0c8ab0d881c7266e2fe4a403454a9a5
PetersGlory <peterglo2200@gmail.com>
2021-12-02 13:14:31 +0100

    first commit by Peter

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from './subComponents/Navbar';
+
+class Home extends React.Component {
+    render() { 
+        return <>
+            <Navbar />
+        </>;
+    }
+}
+ 
+export default Home;
